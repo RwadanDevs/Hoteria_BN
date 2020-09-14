0a17419d0593f6b2b1f00efa65c03d29b11ee291
kabundege <christophekwizera1@gmail.com>
2020-09-15 00:34:52 +0200

    real time authentication #socket.js

--- server/helpers/socketConnection.js
+++ server/helpers/socketConnection.js
@@ -7,10 +7,24 @@ export let io = socketIO(name)
 export const startSocket = (server) => {
   io = socketIO(server);
 
+  const ActiveUsers = [];
+
   io.on('connection', socket => {
-      console.log("User Connected");
+      socket.emit('ping',socket.id)
+
+      socket.on('auth',data=>{
+        let foundUser = false
+        for(const user of ActiveUsers){
+          if(user.authInfo === data.authInfo)
+            foundUser = true;
+        }
+        if(!foundUser)
+        ActiveUsers.push(data);
+      console.log(foundUser,data);
+      })
 
       socket.on('disconnect', () => {
+        console.log("User Disconnected");
         io.emit('Client Disconnected',{ClientId: socket.id})
       })
   });
